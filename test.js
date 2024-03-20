f074f7fadefa58e548655246ba761934f9cda13d
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-03-21 00:22:58 +0530

    first commit

--- test.js
+++ test.js
@@ -1,1 +1,56 @@
-console.log("Hello");
+// const variable1 = "First"
+// const variable2 = "Second"
+// const variable3 = "Third"
+// const variable4 = "Fourth"
+
+// console.table([variable1, variable2, variable3, variable4])
+
+// console.log(typeof variable1) // string
+// console.log(typeof null) // object
+// console.log(typeof undefined) // undefined
+
+// const var1 = 33
+// const convertedVar1 = String(var1)
+// console.log(convertedVar1)
+
+// const getName = "arijitbiswas"
+// console.log(getName.charAt(2))
+// console.log(getName.indexOf("i"))
+
+// const newString1 = getName.substring(1, 4)
+// console.log(newString1)
+
+// const newString2 = getName.slice(-9, 4)
+// if indexStart < 0 then indexStart = max(indexStart + str.length, 0)
+// if indexEnd < 0 then indexEnd = max(indexEnd + str.length, 0)
+// console.log(newString2)
+
+// const spacedName = "    arijitbiswas        "
+// trim out starting and ending empty characters
+// console.log(spacedName.trim())
+
+// const url = "https://arijit%20biswas.com/arijit"
+// console.log(url.replace("%20", "-"))
+
+// console.log(url.includes("arijit"))
+
+// const splitString = "ARIJIT-BISWAS-arijit-biswas"
+// console.log(splitString.split("-"))
+
+// const balance = 400.123456
+// console.log(balance.toFixed(2))
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString("en-IN"))
+
+// const minLimit = 10
+// const maxLimit = 20
+// console.log(Math.round(Math.random() * (maxLimit - minLimit + 1)) + minLimit)
+
+// const customDate = new Date(2024, 2, 21, 24, 60, 60)
+// console.log(customDate.toDateString())
+// console.log(customDate.toLocaleDateString())
+// console.log(customDate.toLocaleString())
+// const customTime = new Date("03-21-2024")
+// const currentTime = Date.now()
+// console.log(currentTime - customTime.getTime())
