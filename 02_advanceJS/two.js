a1e4e0f4c57627a1628979e882f464ab093b60f8
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-03-22 20:01:47 +0530

    first commit

--- 02_advanceJS/two.js
+++ 02_advanceJS/two.js
@@ -45,3 +45,129 @@
 // // DELETE
 // const deleteLang = document.querySelector("li:last-child")
 // deleteLang.remove()
+
+// const firstElement = document.getElementById("Javascript")
+// const secondElement = document.getElementById("Python")
+// firstElement.onclick = () => alert("Clicked")
+// firstElement.addEventListener("click", (e) => console.log(e), false)
+// secondElement.addEventListener(
+//     "click",
+//     (e) => {
+//         e.preventDefault()
+//         e.stopPropagation()
+//         console.log(e)
+//     },
+//     false
+// )
+// document.querySelector("#images").addEventListener("click", (e) => {
+//     if (e.target.tagName === "IMG") {
+//         const removeLi = e.target.parentNode
+//         removeLi.remove()
+//     }
+// })
+
+// const changeText = setTimeout(() => {
+//     document.querySelector("li").innerHTML = "Python"
+// }, 3000)
+// const button = document.getElementById("stop")
+// document.addEventListener("click", (e) => {
+//     console.log("Text change stopped")
+//     clearTimeout(changeText)
+// })
+// const randomColor = () => {
+//     const hexCode = "0123456789ABCDEF"
+//     let hexColor = "#"
+//     for (let i = 0; i < 6; i++) {
+//         hexColor += hexCode[Math.floor(Math.random() * 16)]
+//     }
+//     return hexColor
+// }
+// let startInterval = null
+// document.getElementById("start").addEventListener("click", () => {
+//     if (!startInterval) {
+//         startInterval = setInterval(() => {
+//             document.querySelector("li").innerHTML = randomColor()
+//         }, 1000)
+//     }
+// })
+// document.getElementById("stop").addEventListener("click", () => {
+//     if (startInterval) {
+//         clearInterval(startInterval)
+//         startInterval = null
+//     }
+// })
+
+// const promiseOne = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("Async task is completed")
+//         resolve()
+//     }, 2000)
+// })
+// promiseOne.then(() => {
+//     console.log("Promise consumed")
+// })
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const userObject = {
+//             username: "arijit",
+//             age: 22
+//         }
+//         resolve(userObject)
+//     }, 1000)
+// }).then((userObjectReceived) => {
+//     console.log(userObjectReceived)
+// })
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const error = false
+//         const userObject = {
+//             username: "Arijit",
+//             age: 22
+//         }
+//         const errorObject = {
+//             errorMessage: "ErrorMessage"
+//         }
+//         if (!error) resolve(userObject)
+//         else reject(errorObject)
+//     }, 1000)
+// })
+//     .then((userObjectReceived) => {
+//         return userObjectReceived.username
+//     })
+//     .then((userNameReceived) => {
+//         console.log(userNameReceived)
+//     })
+//     .catch((error) => {
+//         console.log(error.errorMessage)
+//     })
+//     .finally(() => console.log("Finally"))
+// const newPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const error = true
+//         const userObject = {
+//             username: "Arijit",
+//             password: "Biswas"
+//         }
+//         const errorObject = {
+//             errorMessage: "ErrorMessage"
+//         }
+//         if (!error) resolve(userObject)
+//         else reject(errorObject)
+//     }, 1000)
+// })
+// async function consumedPromise() {
+//     try {
+//         const response = await newPromise
+//         console.log(response)
+//     } catch (error) {
+//         console.log(error.errorMessage)
+//     }
+// }
+// consumedPromise()
+
+// fetch("https://fakestoreapi.com/products/1")
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log(error))
